8eab29eab41f2887b6b404fd5a4fd689ee6b761a
Harold F Murcia <harold.murcia@unibague.edu.co>
2018-04-01 16:20:45 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Mercury Challenge
+
+**Mercury Challenge** is a repository with the programing elements based on Robotic Operative System ROS and python to control a 4-wheel reover designed to compete in the LatinAmerican [MercuryChallenge](https://mercurylatino.weebly.com/uploads/4/9/3/7/49370873/reglamento_v6_24012018.pdf).
+
+**Hardware and associate repositories:**
+* Ion Motion Roboclaw, Is Dual DC motor driver: [Lib](http://www.ionmc.com/downloads)
+* Pololu USB servo Control [POL-1353](https://www.pololu.com/product/1353)
+* Hokuyo URG-04LX, [driver](http://wiki.ros.org/hokuyo_node)
+* Raspberry PI 3+
+
+## Getting Started
+
+The following steps describe how to install in the raspberry pi the Mercury-Control.
+* Install ROS: http://wiki.ros.org/kinetic/Installation/Ubuntu
+* Install Joyd: http://wiki.ros.org/joy/Tutorials/ConfiguringALinuxJoystick
+* Install Git: sudo apt-get install git-core
+* Install GPIO: sudo apt-get install python-rpi.gpio [25,28, 29]
+* Install Requests: pip install requests
+
+## USB Rules
+* udevadm info -a -n /dev/ttyUSB1 | grep
+* sudo nano /etc/udev/rules.d/99-robot.rules
+* SUBSYSTEM=="tty", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2404",  SYMLINK+="tty_roboclaw"
+* SUBSYSTEM=="tty", ATTRS{idVendor}=="0424", ATTRS{idProduct}=="9514",  SYMLINK+="tty_pololu"
+* sudo udevadm trigger
+* sudo chmod 666 /dev/tty_roboclaw
+* sudo chmod 666 /dev/tty_pololu
+
+## Bash Functions
+### On RPi
+* export ROS_IP=<Rpi ip>
+* export ROS_MASTER_URI=http://<Rpi ip>:11311
+* sudo chmod 666 /dev/tty_roboclaw
+* sudo chmod 666 /dev/tty_pololu
+* roscore &
+* python com.py &
+* python control.py &
+
+### On Remote
+* export ROS_IP=<remote ip>
+* export ROS_MASTER_URI=http://<Rpi ip>:11311
+* rosparam set joy_node/dev "/dev/input/js1"
+* rosrun joy joy_node &
+
+
+
+
---
+++ com.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+## Simple talker demo that published std_msgs/Strings messages
+## to the 'chatter' topic
+
+import rospy
+from std_msgs.msg import String
+import socket
+import time
+#import RPi.GPIO as GPIO
+#GPIO.setmode(GPIO.BCM)
+
+InternetFail = 0
+urlTest = urlTest='https://www.google.com/'
+timeoutTest=1
+#GPIO.setup(26, GPIO.OUT)
+#GPIO.setup(20, GPIO.OUT)
+#GPIO.setup(21, GPIO.OUT)
+#GPIO.setup(17,GPIO.OUT)
+#Index = GPIO.PWM(17,500)
+#Index.start(0)
+time.sleep(3)
+
+def talker():
+    global InternetFail, urlTest, timeoutTest
+    pub = rospy.Publisher('url_test', String, queue_size=10)
+    rospy.init_node('communications', anonymous=True)
+    rospy.loginfo("Testing URL")
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        try:
+            r = socket.create_connection(("www.google.com", 80))
+            Internet_test = "Internet Ok %s" % rospy.get_time()
+            rospy.loginfo(Internet_test)
+            pub.publish(Internet_test)
+            InternetFail=0
+            OK()
+        except:
+            Internet_test = "Internet FAIL %s" % rospy.get_time()
+            rospy.loginfo(Internet_test)
+            pub.publish(Internet_test)
+            InternetFail=InternetFail+1
+            n = 1;
+            if(InternetFail>=n):
+                print("-------------------------------")
+                print("No internet " + str(n) + " 2 times. \n")
+                print("-------------------------------")
+                noConnection()
+            pass
+        rate.sleep()
+
+def noConnection():
+    print "PAILA"
+ #   GPIO.output(21, 1)  # turn on
+ #   time.sleep(0.5)
+ #   GPIO.output(21, 0)  # turn off
+ #   time.sleep(0.5)
+ #   GPIO.output(21, 1)  # turn on
+
+def OK():
+    print "bien"
+ #   GPIO.output(26, 0)
+ #   GPIO.output(20, 0)
+ #   GPIO.output(21, 0)
+ #   Index.ChangeDutyCycle(50)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
